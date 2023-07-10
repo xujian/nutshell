ac2898e9d00a2c182e938f990c8efb0af726639d
xujian <xujian@ddjf.com.cn>
2023-07-10 11:58:04 +0800

    + rollup-plugin-dts / typings

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,20 +1,31 @@
 import vue from 'rollup-plugin-vue'
 import typescript from 'rollup-plugin-typescript2'
+import dts from 'rollup-plugin-dts'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      format: 'esm',
-      file: 'dist/nutshell.esm.mjs'
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        format: 'esm',
+        file: 'dist/nutshell.esm.mjs'
+      },
+      {
+        format: 'cjs',
+        file: 'dist/nutshell.cjs.js'
+      }
+    ],
+    plugins: [
+      typescript(),
+      vue(),
+    ]
+  },
+  {
+    input: './dist/index.d.ts',
+    output: {
+      file: 'dist/nutshell.d.ts',
+      format: 'es',
     },
-    {
-      format: 'cjs',
-      file: 'dist/nutshell.cjs.js'
-    }
-  ],
-  plugins: [
-    typescript(),
-    vue(),
-  ]
-}
+    plugins: [dts()]
+  }
+]
