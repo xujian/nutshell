f4a2b231a6d35f3f7003f9d118b9048e3336a8d2
xujian <xujian@ddjf.com.cn>
2023-07-24 11:07:31 +0800

    [build] tweak rollup configs

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,19 +1,22 @@
 import dts from 'rollup-plugin-dts'
 import vueJsx from 'rollup-plugin-vue-jsx-compat'
 import esbuild from 'rollup-plugin-esbuild'
+import { watch } from 'vue'
 
 export default [
   {
     input: 'src/index.ts',
     output: [
       {
-        format: 'esm',
-        file: 'dist/nutshell.esm.mjs'
-      },
-      {
         format: 'cjs',
         file: 'dist/nutshell.cjs.js'
-      }
+      },
+    ],
+    output: [
+      {
+        format: 'es',
+        file: 'dist/nutshell.js',
+      },
     ],
     plugins: [
       vueJsx(),
