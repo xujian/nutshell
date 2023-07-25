d28b908f653fc37adc5eb77018ea5294c6279af9
xujian <xujian@ddjf.com.cn>
2023-07-25 17:35:35 +0800

    dummy did works

--- ui/src/providers/nutui/index.ts
+++ ui/src/providers/nutui/index.ts
@@ -2,16 +2,22 @@ import { getCurrentInstance, h } from 'vue'
 import { CoreProvider } from '..'
 import * as components from './components'
 
+const makeDummy = (name: string) => {
+  return () => dummy(name.toUpperCase())
+}
+
 // fallback for component not implemented
-const dummy = () => h('div', {
-  class: 'dummy'
-}, '尚未实现')
+const dummy = (name: string) => {
+  return h('div', {
+    class: 'dummy'
+  }, `NS-${name} 尚未实现`)
+}
 
 const NutuiProvider: CoreProvider = (props: any) => {
   const { parent } = getCurrentInstance()
   const name = parent.type.name.slice(2).toLowerCase()
-  const component = components[name] || dummy
-  return h(component, {...props}, 'Provider works')
+  const component = components[name] || makeDummy(name)
+  return h(component, {...props})
 }
 
 export default NutuiProvider
