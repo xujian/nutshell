bc51f0136bc4c109e67339e3246c2125a249c9bd
xujian <xujian@ddjf.com.cn>
2023-07-25 16:27:52 +0800

    + nutui type alias

--- ui/src/providers/nutui/components/button/index.ts
+++ ui/src/providers/nutui/components/button/index.ts
@@ -1,7 +1,7 @@
 import { h } from 'vue'
 import { Button } from '@nutui/nutui-taro'
 import type { ButtonProps } from 'src/components'
-import { ButtonType } from '@nutui/nutui-taro/dist/types/__VUE/button/type'
+import { ButtonType } from '@nutui-types/button/type'
 
 export const button = (props: ButtonProps) => {
   return h(Button, {
