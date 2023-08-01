28abad9b69e8983977bbc22d8b5d056f2074bd0c
xujian <xujian@ddjf.com.cn>
2023-08-01 17:24:45 +0800

    chore - @nutui/ ConfigProvider

--- docs/src/app.ts
+++ docs/src/app.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { Nutshell } from 'nutshell'
 import '@/styles/app.scss'
-import { ConfigProvider } from '@nutui/nutui-taro'
 
 import router from './router'
 
@@ -11,7 +10,6 @@ const app = createApp({})
 
 app
   .use(router)
-  .use(ConfigProvider)
   .use(nutshell)
 
 export default app
