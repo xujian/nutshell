5f6f20c7e142b270be271e2a89b5b15ba0db2508
xujian <mikejianxu@gmail.com>
2023-09-07 19:41:36 +0800

    fix <ns-chip> classes

--- ui/src/vendors/nutui/components/chip/Chip.ts
+++ ui/src/vendors/nutui/components/chip/Chip.ts
@@ -1,10 +1,11 @@
 import { h } from 'vue'
 import type { ChipProps } from '../../../../components'
+import { MarginProps } from '../../../../utils'
 
-export const Chip = (props: ChipProps) => {
+export const Chip = (props: ChipProps & MarginProps) => {
   const { label, color, textColor } = props
   return h(NutTag, {
-    class: 'ns-chip',
+    class: ['ns-chip'].concat(props.classes),
     round: true,
   }, () => label)
 }
