0bb9856080dc25530c6bb17966f4d960140baf56
xujian <mikejianxu@gmail.com>
2023-09-07 19:41:07 +0800

    fix <ns-chip> color & variant

--- ui/src/components/chip/Chip.ts
+++ ui/src/components/chip/Chip.ts
@@ -1,6 +1,6 @@
 import { buildProps } from '../../utils/private/props'
 import { Color } from '../../composables'
-import { useSizeProps } from '../../props'
+import { useSizeProps, useVariantProps } from '../../props'
 import { define } from '../../utils'
 import { PropType, ExtractPublicPropTypes, ObjectEmitsOptions, SlotsType, ComponentObjectPropsOptions } from 'vue'
 
@@ -23,21 +23,23 @@ export const useChipProps = buildProps({
   textColor: {
     type: String as PropType<Color>,
   },
+  ...useSizeProps(),
+  ...useVariantProps()
 })
 
-const props = {
+export const chipProps = {
   ...useChipProps(),
   ...useSizeProps(),
 }
 
-export type ChipProps = ExtractPublicPropTypes<typeof props>
+export type ChipProps = ExtractPublicPropTypes<typeof chipProps>
 
 /**
  * 标签条 <ns-chip>
  */
 export const NsChip = define({
     name: 'NsChip',
-    props,
+    props: chipProps,
     setup (props, ctx) {
       // 对参数做前期的处理
       return {
