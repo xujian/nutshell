412140ff11a905f102a140f516dcfc6b444727a2
xujian <mikejianxu@gmail.com>
2023-09-13 20:17:54 +0800

    + <regular-form>

--- docs/src/examples/RegularForm.vue
+++ docs/src/examples/RegularForm.vue
@@ -1,27 +1,33 @@
 <template>
-  <ns-form name="client" variant="outlined">
-    <h3 class="mt-xl mb-lg">基础信息</h3>
-    <ns-input name="name"
-      label="客户姓名"
-      v-model="formData.name" />
-    <ns-input name="id"
-      label="证件号码"
-      v-model="formData.id" />
-    <ns-select name="marriage"
-      label="婚姻状态"
-      v-model="formData.marrage"
-      :options="marrageOptions" />
-    <ns-input name="mobile"
-      label="手机号"
-      v-model="formData.mobile" />
-    <h3 class="mt-xl mb-lg">基础信息</h3>
-    <ns-rating label="意向等级"
-      v-model="formData.intention" />
-  </ns-form>
+  <ns-card title="新建意向客户">
+    <ns-form name="client" variant="outlined">
+      <h3 class="mt-xl mb-lg">基础信息</h3>
+      <ns-input name="name"
+        label="客户姓名"
+        v-model="formData.name" />
+      <ns-id-input name="id"
+        label="证件号码"
+        v-model="formData.id" />
+      <ns-select name="marriage"
+        label="婚姻状态"
+        v-model="formData.marrage"
+        :options="marrageOptions" />
+      <ns-mobile-input name="mobile"
+        label="手机号"
+        v-model="formData.mobile" />
+      <h3 class="mt-xl mb-lg">基础信息</h3>
+      <ns-rating label="意向等级"
+        v-model="formData.intention" />
+      <ns-chips label="客户标签"
+        :options="chipsOptions"
+        v-model="formData.tags" />
+    </ns-form>
+  </ns-card>
 </template>
 
 <script lang="ts" setup>
 import { reactive } from 'vue'
+import type { LabelValuePair } from 'nutshell'
 
 const formData = reactive({
   name: '',
@@ -32,7 +38,16 @@ const formData = reactive({
   tags: []
 })
 
-const marrageOptions = [
+const chipsOptions: LabelValuePair[] = [
+  { label: '有车', value: 'YC' },
+  { label: '保单', value: 'BD' },
+  { label: '有经营公司', value: 'YJYGS' },
+  { label: '未加微信', value: 'WJWX' },
+  { label: '公积金', value: 'GJJ' },
+  { label: '黑名单', value: 'HMD' },
+]
+
+const marrageOptions: LabelValuePair[] = [
   { label: '未婚', value: 'married' },
   { label: '已婚', value: 'unmarried' },
   { label: '离异', value: 'divorced' },
--- docs/src/pages/components/FormPage.vue
+++ docs/src/pages/components/FormPage.vue
@@ -1,11 +1,34 @@
 <template>
   <h1 class="my-lg">表单 &lt;ns-form&gt;</h1>
-  <h2>新建意向客户</h2>
   <regular-form />
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <h2>用户输入校验</h2>
+  <ns-form>
+    <ns-input name="clientName" v-model="userName" label="客户名称" :rules="['required']" />
+    <ns-select name="clientLocation" v-model="userLocation"
+      :options="cities"
+      label="区域" :rules="['required']" />
+  </ns-form>
 </template>
 
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
 import RegularForm from '../../examples/RegularForm.vue'
+
+const userName = ref('')
+const userLocation = ref('')
+const cities = ref<any[]>([])
+
+onMounted(async () => {
+  axios.get('/json/cities.json')
+    .then(response => response.data)
+    .then(data => data.map(d => ({value: d.id, label: d.name})))
+    .then(data => {
+      cities.value = data
+    })
+})
 </script>
 
 <style lang="scss">
