87644bd700f02716932ed578734914553d59b215
xujian <mikejianxu@gmail.com>
2023-09-14 20:46:18 +0800

    + color shades

--- docs/src/pages/themes/PalettesPage.vue
+++ docs/src/pages/themes/PalettesPage.vue
@@ -1,13 +1,37 @@
 <template>
   <h1 class="my-lg">标准色表</h1>
-  <ns-row v-for="c in brands" :key="c.name" justify="start" :gutter="16">
+  <ns-row v-for="c in brands" :key="c.name" justify="start" align="start" :gutter="16">
     <ns-col class="m-sm">
       <color-card :color="c"></color-card>
+    </ns-col>
+    <ns-col class="shades py-md">
+      <ns-row class="tints-row r-sm mb-xs">
+        <div class="color"
+          v-for="n of shades" :key="n"
+          v-tooltip="`${c.name}-${n}`"
+          :style="{backgroundColor: `var(--ns-${c.name}-${n})`}"></div>
+      </ns-row>
+      <ns-row class="shades-row r-sm mb-xs">
+        <div class="color"
+          v-for="n of shades" :key="n"
+          v-tooltip="`${c.name}-${n}`"
+          :style="{backgroundColor: `var(--ns-${c.name}--${n})`}"></div>
+      </ns-row>
+      <ns-row class="alphas-row r-sm mb-xs">
+        <div class="color"
+          v-for="n of shades" :key="n"
+          v-tooltip="`${c.name}---${n}`"
+          :style="{backgroundColor: `var(--ns-${c.name}---${n})`}"></div>
+      </ns-row>
     </ns-col>
   </ns-row>
 </template>
 <script lang="ts" setup>
 import ColorCard from '@/components/ColorCard.vue'
+
+const shades = [
+  10, 20, 30, 40, 50, 60, 70, 80, 90
+]
 
 const brands = [
   {
@@ -41,3 +65,18 @@ const brands = [
 ]
 
 </script>
+
+<style lang="scss">
+.shades {
+  .tints-row, .shades-row, .alphas-row {
+    border-style: solid;
+    border-width: 1px;
+    border-color: var(--ns-primary---50);
+    .color {
+      width: 32px;
+      height: 32px;
+    }
+    overflow: hidden;
+  }
+}
+</style>
