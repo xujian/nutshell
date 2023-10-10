82ac236b25a7a26a677004e5b943af23745c5a8f
xujian <mikejianxu@gmail.com>
2023-10-10 11:46:26 +0800

    fix build error

--- ui/src/vendors/nutui/components/button/Button.ts
+++ ui/src/vendors/nutui/components/button/Button.ts
@@ -1,7 +1,7 @@
 import { h } from 'vue'
 import type { ButtonEmits, ButtonProps } from '../../../../components'
 import { Size } from '../../../../props/size'
-import { ButtonSize } from '@nutui/nutui-taro/dist/types/__VUE/button/type'
+import { ButtonSize, ButtonShape } from '@nutui/nutui-taro/dist/types/__VUE/button/type'
 import { BRANDS, BrandColor } from '../../../../composables/theme'
 import { MarginProps } from '../../../../utils'
 
@@ -50,8 +50,8 @@ export const Button = (props: ButtonProps & ButtonEmits & MarginProps) => {
   return h(NutButton, {
     class: classes,
     disabled,
-    shape: 'suqare',
-    ...!colorIsBrand && {color},
+    shape: 'suqare' as ButtonShape,
+    ...!colorIsBrand && {color: color as string},
     size: getSize(props.size),
     onClick: props.onClick
   }, {
