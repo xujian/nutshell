a1b3c94c3839f21e08ca8ccf04d809832d63706e
杨龙 <yanglong@ddjf.com.cn>
2023-10-11 18:59:06 +0800

    fix: 时间格式处理

--- ui/src/vendors/antdv/components/table/columns/datetime.ts
+++ ui/src/vendors/antdv/components/table/columns/datetime.ts
@@ -13,7 +13,7 @@ export default function button (
   const formatRangedDateTime = (input: number | string): string => {
     if (!input) return ''
     const timeValue = dayjs(+input),
-      date = timeValue.format(props?.format || 'YYYY-MM-DD HH:MM')
+      date = timeValue.format(props?.format || 'YYYY-MM-DD HH:mm:ss')
     return `${date}`
   }
   return ({text, record, index}) => h('div', {
