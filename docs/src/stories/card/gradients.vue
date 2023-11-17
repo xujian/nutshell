68a30e4ed2adff3a71eb14d68eaa5abc4c92094a
xujian <mikejianxu@gmail.com>
2023-11-17 18:33:24 +0800

    + card story gradient

---
+++ docs/src/stories/card/gradients.vue
@@ -0,0 +1,6 @@
+<template>
+  <ns-card class="gradient" gradient="primary,#ffffff00/180"></ns-card>
+  <ns-card class="gradient" gradient="#FF57B9,#A704FD"></ns-card>
+  <ns-card class="gradient" gradient="#c3ec52,#0ba29d/90"></ns-card>
+  <ns-card class="gradient" gradient="#f2d50f,#da0641/135"></ns-card>
+</template>
