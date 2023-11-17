dbacaf4f1d7d501a452088beabbcb5f10373e2f2
杨龙 <yanglong@ddjf.com.cn>
2023-11-17 11:49:33 +0800

    fix: 代码缩进调整

--- ui/src/components/switch/Switch.ts
+++ ui/src/components/switch/Switch.ts
@@ -3,29 +3,29 @@ import { define, MakePropsType } from '../../utils'
 import { useModelValuePropsForBoolean } from '../../props'
 
 export const Props = {
-    // antd/nutui支持string和number，但我觉得没必要，布尔值就可以了
-    ...useModelValuePropsForBoolean(),
-    disabled: {
-        type: Boolean,
-        default: false,
-    },
-    // TODO -- nico 这块size有待商榷
-    size: {
-        type: String as PropType<'default' | 'small'>,
-        default: 'default'
-    }
+  // antd/nutui支持string和number，但我觉得没必要，布尔值就可以了
+  ...useModelValuePropsForBoolean(),
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  // TODO -- nico 这块size有待商榷
+  size: {
+    type: String as PropType<'default' | 'small'>,
+    default: 'default'
+  }
 }
 
 export interface SwitchEmits extends ObjectEmitsOptions {
-    'change': (checked: boolean, event: Event) => void
+  'change': (checked: boolean, event: Event) => void
 }
 
 const emits: SwitchEmits = {
-    'change': (checked: boolean, event: Event) => void 0
+  'change': (checked: boolean, event: Event) => void 0
 }
 
 export interface SwitchSlots extends SlotsType {
-    default: never,
+  default: never,
 }
 
 export type SwitchProps = MakePropsType<typeof Props, SwitchEmits>
@@ -34,13 +34,13 @@ export type SwitchProps = MakePropsType<typeof Props, SwitchEmits>
  * Switch 组件 <ns-switch>
  */
 export const NsSwitch = define({
-    name: 'NsSwitch',
-    props: Props,
-    emits,
-    setup (props, ctx) {
-        // console.log('components/switch', props)
-        return {
-        }
+  name: 'NsSwitch',
+  props: Props,
+  emits,
+  setup (props, ctx) {
+    // console.log('components/switch', props)
+    return {
     }
+  }
 })
 // 需要增加 import 到 ./index.ts, ../components.ts
--- ui/src/vendors/antdv/components/switch/Switch.ts
+++ ui/src/vendors/antdv/components/switch/Switch.ts
@@ -3,20 +3,20 @@ import { Switch as AntdvSwitch } from 'ant-design-vue'
 import { SwitchProps } from '../../../../components'
 
 export const Switch = (props: SwitchProps, ctx: SetupContext) => {
-    // console.log('vendors/switch', props)
+  // console.log('vendors/switch', props)
 
-    return h(AntdvSwitch, {
-        class: 'ns-switch',
-        checked: props.modelValue,
-        disabled: props.disabled,
-        size: props.size,
-        'onUpdate:checked': (value: boolean | string | number) => {
-            if (typeof value === 'boolean') {
-                props['onUpdate:modelValue']?.(value as boolean)
-            } else {
-                throw new Error('请使用Boolean值绑定')
-            }
-        }
-    }, () => '')
+  return h(AntdvSwitch, {
+    class: 'ns-switch',
+    checked: props.modelValue,
+    disabled: props.disabled,
+    size: props.size,
+    'onUpdate:checked': (value: boolean | string | number) => {
+      if (typeof value === 'boolean') {
+        props['onUpdate:modelValue']?.(value as boolean)
+      } else {
+        throw new Error('请使用Boolean值绑定')
+      }
+    }
+  }, () => '')
 }
 // + import => ./index.ts, ../components.ts
