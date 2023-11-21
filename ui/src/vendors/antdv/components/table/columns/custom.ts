1f983451272016d1b1e6ae4ba4417b19d165dfbe
xujian <mikejianxu@gmail.com>
2023-11-21 18:11:25 +0800

    <ns-table-column> template #content={row, rowIndex}

--- ui/src/vendors/antdv/components/table/columns/custom.ts
+++ ui/src/vendors/antdv/components/table/columns/custom.ts
@@ -3,21 +3,20 @@ import { SetupContext, h } from 'vue'
 
 /**
  * Table column: custom
- * @param column 
- * @param custom 
+ * @param column
+ * @param custom
  */
 export default function custom (
     props: TableColumnCustomProps
   ): CustomColumnFunctionalRender {
     return ({row, rowIndex, columnIndex}: TableColumnData, ctx?: SetupContext) => {
-      console.log('custom----table c0olumn', rowIndex, columnIndex);
-      
-      return h('div', 
+      // console.log('custom----table c0olumn', rowIndex, columnIndex)
+      return h('div',
         {
           class: 'table-column-custom-content',
         },
         {
-          default: () => ctx?.slots?.content?.(row)
+          default: () => ctx?.slots?.content?.({row, rowIndex, columnIndex})
         }
       )
     }
