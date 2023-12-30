f7ea7d6f9f149c5a3b84fca0dff51fe4a2136938
xujian <mikejianxu@gmail.com>
2023-12-30 13:06:54 +0800

    + VeturData

---
+++ docs/src/components/VeturData.vue
@@ -0,0 +1,10 @@
+<template>
+  <pre>
+    {{ veturData }}
+  </pre>
+</template>
+
+<script lang="ts" setup>
+import { properties } from '@uxda/nutshell'
+const veturData = JSON.stringify(properties, null, 2)
+</script>
