f49d89eab424ef02c9a5719b842675d5402186df
xujian <mikejianxu@gmail.com>
2024-01-19 17:07:38 +0800

    + stories/tabbar

--- docs/src/pages/components/TabbarPage.vue
+++ docs/src/pages/components/TabbarPage.vue
@@ -2,27 +2,12 @@
   <h1 class="my-lg">Tabbar &lt;ns-tabbar&gt;</h1>
   <h3>基本特性</h3>
   <p>&nbsp;</p>
-  <ns-tabbar class="demo-tabbar"
-    :items="items">
-  </ns-tabbar>
+  <story file="tabbar/basic.vue" />
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
-
-const items = [
-  {
-    label: '首页',
-    icon: 'house'
-  },
-  {
-    label: '账户',
-    icon: 'gear'
-  },
-]
 </script>
 
 <style lang="scss">
-.demo-tabbar {
-}
 </style>
---
+++ docs/src/stories/tabbar/basic.vue
@@ -0,0 +1,21 @@
+<template>
+  <ns-tabbar class="basic-tabbar"
+    :items="items">
+  </ns-tabbar>
+</template>
+<script lang="ts" setup>
+const items = [
+  {
+    label: '首页',
+    icon: 'house'
+  },
+  {
+    label: '账户',
+    icon: 'gear'
+  },
+]
+</script>
+<style lang="scss">
+.basic-tabbar {
+}
+</style>
