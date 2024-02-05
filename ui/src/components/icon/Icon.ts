d82db4b56393cd82032c7587f19a9bfabb5a23fa
xujian <mikejianxu@gmail.com>
2024-02-05 16:36:40 +0800

    <ns-icon> + format

--- ui/src/components/icon/Icon.ts
+++ ui/src/components/icon/Icon.ts
@@ -1,13 +1,31 @@
-import { ObjectEmitsOptions, PropType, defineComponent } from 'vue'
+import { ObjectEmitsOptions, PropType, defineComponent, h, VNode } from 'vue'
 import { define, MakePropsType } from '../../utils'
 import { Color } from '../../composables'
 import { useSizeProps } from '../../props/size'
 import { buildProps } from '../../utils/private/props'
-import { h } from 'vue'
+import { useNutshell } from '../../framework'
+
+export type IconFormat =
+  /**
+   * Icon font + CSS
+   */
+  'font' |
+  /**
+   * SVG Sprite
+   */
+  'sprite' |
+  /**
+   * 配色 SVG (用于小程序)
+   */
+  'svg'
 
 export const useIconProps = buildProps({
   name: {
     type: String,
+  },
+  format: {
+    type: String as PropType<IconFormat>,
+    default: () => 'sprite',
   },
   /**
    * SVG或图片URI
@@ -39,6 +57,35 @@ const emits: IconEmits = {
 export type IconProps = MakePropsType<typeof props, IconEmits>
 
 /**
+ * 按照 format 输出对应的格式
+ */
+const formats: Record<IconFormat, (props: IconProps) => VNode> = {
+  font: (props: IconProps) => h('i', {
+      class: [
+        'ns-icon',
+        'ni',
+        `ni-${props.name}`,
+        `font-size-${props.size}`
+      ]
+    }),
+  sprite: (props: IconProps) => h('svg', {
+      class: [
+        'ns-icon',
+        `font-size-${props.size}`
+      ]
+    }, h('use', {
+      href: `http://localhost:2024/sprite.svg#${props.name}`
+    })),
+  svg:  (props: IconProps) => h('img', {
+      class: [
+        'ns-icon',
+        props.size && `font-size-${props.size}`
+      ],
+      src: `http://localhost:2024/icons/default/${props.name}.svg`
+    }),
+}
+
+/**
  * 图标组件
  */
 export const NsIcon = defineComponent({
@@ -46,13 +93,10 @@ export const NsIcon = defineComponent({
   props,
   emits,
   setup (props, ctx) {
-    return () => h('i', {
-      class: [
-        'ns-icon',
-        'ni',
-        `ni-${props.name}`,
-        `font-size-${props.size}`
-      ]
-    })
+    const $n = useNutshell()
+    const format = props.format
+      || $n?.options.icon
+      || 'sprite'
+    return () => formats[format](props)
   }
 })
