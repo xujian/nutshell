92857489fb05101632e5e816dc2ad7cf346bba76
xujian <mikejianxu@gmail.com>
2024-02-05 16:39:25 +0800

    fix tabbar icon

--- expo/mobile/src/custom-tab-bar/index.vue
+++ expo/mobile/src/custom-tab-bar/index.vue
@@ -1,27 +1,41 @@
 <template>
   <ns-tabbar class="basic-tabbar"
-    :items="items">
+    :items="items"
+    :style="style">
   </ns-tabbar>
 </template>
 <script lang="ts" setup>
-import './index.scss'
+import { useSafeArea } from '@uxda/appkit-next'
+
+const { bottom } = useSafeArea()
+
+// 使用 useSafeAre 避开底部的操作条
+const style = {
+  paddingBottom: `${bottom}px`
+}
 
 const items = [
   {
     label: '首页',
-    icon: 'house'
+    icon: 'home'
   },
   {
     label: '账户',
-    icon: 'gear'
+    icon: 'account'
   },
 ]
 </script>
 <style lang="scss">
+@import url('./index.scss');
+
 .basic-tabbar {
   --font-size-sm: 14px;
   --icon-md: 16px;
   border-top: solid 1px #ddd;
+  .ns-icon {
+    width: 24px;
+    height: 24px;
+  }
   font-size: 12px;
   .ni-icon {
     --font-size-sm: 16px;
