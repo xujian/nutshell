1fbdf50f580251627592faa2ee64969589912468
gaozhu <gaozhu@ddjf.com.cn>
2024-03-28 17:43:42 +0800

    + Cascader placeholder

--- ui/src/components/input/MobileInput.ts
+++ ui/src/components/input/MobileInput.ts
@@ -2,11 +2,9 @@ import { h } from 'vue'
 import { InputProps, InputType, NsInput } from './Input'
 
 const defaultProps: Partial<InputProps> = {
-  type: 'number',
   maxlength: 11,
   rules: ['mobile']
 }
-
 
 /**
  * 手机号输入框 <ns-mobile-input>
--- ui/src/vendors/antdv/components/select/CascadingSelect.ts
+++ ui/src/vendors/antdv/components/select/CascadingSelect.ts
@@ -8,37 +8,37 @@ import { ValueType } from 'ant-design-vue/es/vc-cascader/Cascader'
 export const CascadingSelect = defineComponent({
   name: 'AntdvCascadingSelect',
   props: cascadingSelectProps,
-  setup (props, ctx) {
+  setup(props, ctx) {
+    const classes = ['ns-select', 'ns-cascading-select']
 
-    const classes = [
-      'ns-select',
-      'ns-cascading-select',
-    ]
+    const { clearable, searchable } = props
 
-    const {
-      clearable, searchable
-    } = props
-  
     const rules = transformRules(props.rules as FullValidationRule[])
 
-    return () => h(AntFormItem, {
-        name: props.name,
-        class: 'ns-form-item',
-        label: props.label,
-        rules
-      }, () => h(AntCascader, {
-        class: classes,
-        name: props.name,
-        options: props.options,
-        allowClear: clearable,
-        showSearch: searchable,
-        value: props.modelValue,
-        'onUpdate:value': (value: ValueType) => {
-          props['onUpdate:modelValue']?.(value as string[])
+    return () =>
+      h(
+        AntFormItem,
+        {
+          name: props.name,
+          class: 'ns-form-item',
+          label: props.label,
+          rules
         },
-        popupClassName: 'ns-select-dropdown',
-        disabled: props.disabled ?? false,
-      })
-    )
+        () =>
+          h(AntCascader, {
+            class: classes,
+            name: props.name,
+            options: props.options,
+            allowClear: clearable,
+            showSearch: searchable,
+            value: props.modelValue,
+            placeholder: props.placeholder,
+            'onUpdate:value': (value: ValueType) => {
+              props['onUpdate:modelValue']?.(value as string[])
+            },
+            popupClassName: 'ns-select-dropdown',
+            disabled: props.disabled ?? false
+          })
+      )
   }
 })
