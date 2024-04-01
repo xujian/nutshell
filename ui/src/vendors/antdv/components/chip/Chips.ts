8b6d04b97f3af73e4170b388c7c82df6b19a7dd8
gaozhu <gaozhu@ddjf.com.cn>
2024-04-01 12:27:49 +0800

    + chips variant

--- ui/src/vendors/antdv/components/chip/Chips.ts
+++ ui/src/vendors/antdv/components/chip/Chips.ts
@@ -4,33 +4,34 @@ import { ChipsProps, NsChip } from '../../../../components'
 import { LabelValuePair } from '../../../../shared/models'
 
 export const Chips = (props: ChipsProps, ctx: SetupContext) => {
-
   const options = props.options || []
 
   const onItemClick = (item: LabelValuePair) => {
     const value = props.modelValue || [],
       itemIncludes = value.includes(item.value),
       newValue = itemIncludes
-        ? value.filter(a => a !== item.value)
+        ? value.filter((a) => a !== item.value)
         : Array.from(new Set([...value, item.value]))
-      props['onUpdate:modelValue']?.(newValue)
+    props['onUpdate:modelValue']?.(newValue)
   }
 
-  return h(AntFormItem, {
-    class: [
-      'ns-chips',
-      'ns-form-item',
-    ],
-    label: props.label,
-  }, () => options.map(o => {
-      const on = props.modelValue && props.modelValue.includes(o.value)
-      return h(NsChip, {
-        class: on ? ['selected'] : [],
-        color: on ? 'primary' : 'neutral',
-        label: o.label,
-        onClick: () => onItemClick(o),
+  return h(
+    AntFormItem,
+    {
+      class: ['ns-chips', 'ns-form-item'],
+      label: props.label
+    },
+    () =>
+      options.map((o) => {
+        const on = props.modelValue && props.modelValue.includes(o.value)
+        return h(NsChip, {
+          class: on ? ['selected'] : [],
+          color: on ? 'primary' : 'neutral',
+          variant: props.variant,
+          label: o.label,
+          onClick: () => onItemClick(o)
+        })
       })
-    })
   )
 }
 // + import => ./index.ts, ../components.ts
