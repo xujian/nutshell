bca7ff99a59a0cb65b4c9f9acbc01035ccbded30
xujian <mikejianxu@gmail.com>
2024-04-02 16:12:35 +0800

    form + custom validate

---
+++ docs/src/stories/form/custom-validate.vue
@@ -0,0 +1,17 @@
+<template>
+  <ns-form v-model="form" >
+    <ns-input label="订单号" v-model="form.order" name="order" :rules="[checkOrder]" />
+  </ns-form>
+</template>
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+const checkOrder = (value: string) => {
+  console.log('===form custom validate', value)
+  return value.startsWith('order-')
+}
+
+const form = reactive({
+  order: '',
+})
+</script>
