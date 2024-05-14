d90e0d8d7c8458f86ef03b0215d9c45672d4ad1f
xujian <mikejianxu@gmail.com>
2024-05-14 11:31:33 +0800

    update DialogPage

--- docs/src/pages/interactive/DialogPage.vue
+++ docs/src/pages/interactive/DialogPage.vue
@@ -2,6 +2,10 @@
   <h1 class="my-lg">弹窗 &lt;ns-dialog&gt;</h1>
   <story file="dialog/basic" />
   <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <h3>弹出子界面</h3>
+  <p class="caption">确定时询问子界面是否允许关闭</p>
+  <p>&nbsp;</p>
   <story file="dialog/component" />
 </template>
 
