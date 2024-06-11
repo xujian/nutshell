7a45e956e3000e9fa4dc233984b78e6077f53fa1
xujian <mikejianxu@gmail.com>
2024-06-11 16:22:02 +0800

    fix vite server proxy

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -5,8 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
-import Markdown from 'vite-plugin-md'
-import TestPlugin from './vite/test-plugin'
 import VirtualPlugin from './vite/virtual-plugin'
 
 const nsRegex = /^Ns[A-Z][\w]+/
@@ -41,7 +39,14 @@ export default defineConfig({
   server: {
     port: 2024,
     open: true,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:2024',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   resolve: {
     alias: {
