684b78635d809c8c396daa4b4b2c87d254e425f8
xujian <mikejianxu@gmail.com>
2024-06-19 18:26:05 +0800

    fix <ns-flex>/<ns-number> styles

---
+++ docs/src/stories/repeator/basic.vue
@@ -0,0 +1,61 @@
+<template>
+  <ns-repeator class="demo-repeator"
+    :items="items"
+    :gutter="10"
+    align="stretch"
+    wrap
+    v-slot="item">
+    <ns-number
+      :value="item.count"
+      :footer="item.name"
+      variant="outlined"
+      justify="center"
+      foreground="#fff"
+      gradient="#e94057,#f27121/45" />
+  </ns-repeator>
+</template>
+
+<script lang="ts" setup>
+const items = [
+  {
+    name: '总计客户',
+    count: 64027
+  },
+  {
+    name: '线索客户',
+    count: 63933
+  },
+  {
+    name: '意向客户',
+    count: 72
+  },
+  {
+    name: '上门客户',
+    count: 12
+  },
+  {
+    name: '签单客户',
+    count: 1
+  },
+  {
+    name: '放款客户',
+    count: 3
+  },
+  {
+    name: '结单客户',
+    count: 3
+  },
+  {
+    name: '总计订单数',
+    count: 2
+  },
+  {
+    name: '总计放款数',
+    count: 2
+  },
+  {
+    name: '总计放款金额/万元',
+    count: 10200
+  },
+]
+</script>
