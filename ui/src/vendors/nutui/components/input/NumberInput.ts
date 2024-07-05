92b6460e0e0048d639842f24f0807e352c5c103d
xujian <mikejianxu@gmail.com>
2024-07-05 16:56:27 +0800

    <ns-number-input> + nutui vendor

---
+++ ui/src/vendors/nutui/components/input/NumberInput.ts
@@ -0,0 +1,43 @@
+import { defineComponent, h } from 'vue'
+import { numberInputEmits, numberInputProps } from '../../../../components'
+import { marginProps } from '../../../../utils'
+import { renderFormItem, useForm } from '../../utils'
+
+export const NumberInput = defineComponent({
+  name: 'NutuiInput',
+  props: {
+    ...numberInputProps,
+    ...marginProps
+  },
+  emits: numberInputEmits,
+  setup (props, { slots }) {
+
+    const form = useForm()
+
+    return () => renderFormItem(props, slots,
+      () => h(NutInput, {
+          border: false,
+          name: props.name,
+          type: 'number',
+          placeholder: props.placeholder,
+          modelValue: props.modelValue,
+          inputAlign: props.variant === 'solid' ? 'left' : 'right',
+          placeholderClass: 'input-placeholder',
+          'onUpdate:modelValue': (value: number | string) => {
+            props['onUpdate:modelValue']?.(value)
+          },
+          onBlur () {
+            form.validate(props)
+            return false
+          }
+        },
+        {
+          ...slots.prepend ? {
+            // @ts-ignore
+            left: () => slots.prepend()
+          } : {}
+        }
+      )
+    )
+  }
+})
--- ui/src/vendors/nutui/components/input/index.ts
+++ ui/src/vendors/nutui/components/input/index.ts
@@ -1,9 +1,11 @@
 import { Input } from './Input'
 import { DateInput } from './DateInput'
 import { DateRangeInput } from './DateRangeInput'
+import { NumberInput } from './NumberInput'
 
 export {
   Input,
   DateInput,
   DateRangeInput,
+  NumberInput,
 }
