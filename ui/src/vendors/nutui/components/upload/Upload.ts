f2a16a82582fb234d5b69d2a2ef6139f7d3dfa44
xujian <mikejianxu@gmail.com>
2024-08-20 19:00:06 +0800

    fix upload

--- ui/src/vendors/nutui/components/upload/Upload.ts
+++ ui/src/vendors/nutui/components/upload/Upload.ts
@@ -1,6 +1,5 @@
 import { defineComponent, h, SetupContext } from 'vue'
 import { uploadEmits, uploadProps, UploadProps } from '../../../../components'
-import { marginProps } from '../../../../utils'
 import { renderFormItem } from '../../utils'
 import { getMediaType, Media } from '../../../../types/media'
 
@@ -11,26 +10,24 @@ export const Upload = defineComponent({
   setup: (props, { slots, emit }) => {
 
     const onClick = () => {
-      const { tempFiles: [image]} = wx.chooseMedia({
+      wx.chooseMedia({
         count: 1,
         mediaType: ['image'],
         soruceType: ['album', 'camera'],
         maxDuration: 30,
         camera: 'back',
-      })
-      console.log('===image', image)
-      props.handler?.({
-        path: image.path,
-      }).then((result: Media) => {
-        const uploaded = {
-          ...result,
-          type: getMediaType(result.name!)
+        success: async (selected: any) => {
+          const result: Media[] = []
+          const {tempFiles: files} = selected
+          for (const f of files) {
+            const media = await props.handler?.({
+              path: f.tempFilePath
+            })
+            if (media) {
+              result.push(media)
+            }
+          }
         }
-        props['onUpdate:modelValue']?.([
-          ...props.modelValue || [],
-          uploaded
-        ])
-        emit('complete', uploaded)
       })
     }
 
