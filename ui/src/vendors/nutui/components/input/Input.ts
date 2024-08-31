cd649101ab16edff15f34b0b2266353299ecd736
gaozhu <gaozhu@ddjf.com.cn>
2024-08-31 16:22:55 +0800

    fix: ns-input disabled

--- ui/src/vendors/nutui/components/input/Input.ts
+++ ui/src/vendors/nutui/components/input/Input.ts
@@ -21,6 +21,7 @@ export const Input = defineComponent({
           name: props.name,
           placeholder: props.placeholder,
           modelValue: props.modelValue,
+          disabled: props.disabled ?? false,
           maxLength: props.maxlength ?? 50,
           inputAlign: props.variant === 'solid' ? 'left' : 'right',
           placeholderClass: 'input-placeholder',
