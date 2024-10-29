a148438e394c86e56db6b3e140286ea696c0f5c6
xujian <mikejianxu@gmail.com>
2024-10-29 09:56:47 +0800

    refactoring SCSS structure

---
+++ ui/src/Taro.ts
@@ -0,0 +1,12 @@
+const Taro: any = {
+  showToast (options: any) {},
+  navigateBack: () => {},
+  getWindowInfo: () => {},
+  getMenuButtonBoundingClientRect: () => {},
+  navigateTo: (options: any) => {},
+  chooseMedia: (options: any) => {}
+}
+
+export {
+  Taro
+}
