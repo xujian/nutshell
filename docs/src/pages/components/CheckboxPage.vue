65ab66c08c3c7253e0309d3bd8391367b1d60644
xujian <mikejianxu@gmail.com>
2024-11-08 15:50:19 +0800

    fix form item styles

--- docs/src/pages/components/CheckboxPage.vue
+++ docs/src/pages/components/CheckboxPage.vue
@@ -1,40 +1,28 @@
 <template>
-  <h1 class="my-lg">复选框 &lt;ns-checkbox&gt;</h1>
-  <h2 class="my-md">Size</h2>
-  <ns-row class="sizes">
-    <ns-checkbox s color="primary"
-      v-for="size in sizes" :key="size"
-      v-model="checked"
-      :label="label" :size="size"></ns-checkbox>
-  </ns-row>
-  <h2 class="my-md">Color</h2>
-  <ns-row class="colors">
-    <ns-checkbox v-for="color in brands" :color="color"
-      v-model="checked"
-      :label="label" :key="color"></ns-checkbox>
-  </ns-row>
-  <h2 class="my-md">Variants</h2>
-  <ns-row class="variants" v-for="(variant) in variants" align="center" :key="variant">
-    <h4 class="variant-label">{{ variant }}</h4>
-    <ns-checkbox v-for="(color) in colors" class="my-xs"
-      :key="color"
-      v-model="checked"
-      :color="color"
-      :variant="variant" :label="label"></ns-checkbox>
-  </ns-row>
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
-  <h3>复选框组</h3>
-  <p>&nbsp;</p>
-  <story file="checkbox/group" />
+  <ns-page class="checkbox-page">
+    <ns-page-content>
+      <h1 class="my-lg">复选框</h1>
+      <p class="caption">&lt;ns-checkbox&gt;/&lt;ns-checkbox-group&gt;</p>
+      <ns-card fill="#fff">
+        <div class="row" justify="start">
+          <ns-checkbox
+            name="check"
+            label="我已阅读并同意用户条款"
+            v-model="agreed" />
+        </div>
+      </ns-card>
+      <h2>复选框组</h2>
+      <p>&nbsp;</p>
+      <story file="checkbox/group" />
+    </ns-page-content>
+  M</ns-page>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { brands, sizes, variants } from '@uxda/nutshell'
 
-const label = '受限用户',
-  checked = ref(false)
+const agreed = ref(false)
 
 </script>
 
