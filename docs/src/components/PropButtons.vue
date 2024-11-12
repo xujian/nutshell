4d437674c2486effb64ad6ee49c349abba854a60
xujian <mikejianxu@gmail.com>
2024-11-12 15:54:34 +0800

    + /docs <prop-buttons>

---
+++ docs/src/components/PropButtons.vue
@@ -0,0 +1,28 @@
+<template>
+  <ns-row class="props-buttons justify-between">
+    <div class="label">填色 <span class="caption">(fill)</span></div>
+    <ns-button-group size="xs" v-model="model" color="primary" :options />
+  </ns-row>
+</template>
+
+<script lang="ts" setup>
+import type { PropType } from 'vue'
+
+
+const model = defineModel()
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  options: {
+    type: Array as PropType<Record<string, any>>,
+    default: []
+  }
+})
+</script>
