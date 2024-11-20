88c0eea7ac2d66126bee2517f4dd1a37c97c59ea
xujian <mikejianxu@gmail.com>
2024-11-20 10:06:50 +0800

    /docs + <note>

---
+++ docs/src/components/Note.vue
@@ -0,0 +1,19 @@
+<template>
+  <ns-card class="note-card" stroke="neutral" :title>
+    <slot></slot>
+  </ns-card>
+</template>
+
+<script lang="ts" setup>
+export type NoteProps = {
+  title: string
+}
+defineProps<NoteProps>();
+
+</script>
+
+<style lang="scss">
+.note-card {
+  background-color: #FFC107;
+}
+</style>
