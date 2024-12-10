c2bcc696771a376c1cfcc3f9c556e37a708d1757
xujian <mikejianxu@gmail.com>
2024-12-10 09:25:27 +0800

    fix <ns-chips> items

--- docs/src/stories/chip/basic.vue
+++ docs/src/stories/chip/basic.vue
@@ -1,9 +1,9 @@
 <template>
-  <ns-chips :options="tags" color="primary" justify="start" />
+  <ns-chips :items color="primary" justify="start" />
 </template>
 
 <script lang="ts" setup>
-const tags = [
+const items = [
     { label: '经营企业', value: '1' },
     { label: '房地产', value: '2' },
     { label: '海外资产', value: '3' },
