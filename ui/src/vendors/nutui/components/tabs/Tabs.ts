494f7afc91506c16a73ae9d1f5e0a899a35ca258
xujian <mikejianxu@gmail.com>
2024-12-12 18:41:17 +0800

    fix slot outside warning

--- ui/src/vendors/nutui/components/tabs/Tabs.ts
+++ ui/src/vendors/nutui/components/tabs/Tabs.ts
@@ -2,26 +2,30 @@ import { h, SetupContext } from 'vue'
 import { TabsItem, TabsProps } from '../../../../components'
 
 export const Tabs = (props: TabsProps, { emit, slots }: Omit<SetupContext, 'expose'>) => {
-  // 确定 children
-  // slots 优先级高于 items
-  // 如果定义了 slots, items 失效
-  // @ts-ignore
-  const items: TabsItem[] = slots.default?.().map((s, index) => {
-    // @ts-ignore
-    const tabSlot = s.children?.tab?.()
-    return {
-      value: s.props?.key as string,
-      label: tabSlot || s.props?.tab || `Tab-${index}` as string,
-      content: s.children
-    }
-  }) || props.items || []
 
-  const defaultSlot = () => items?.map(item => h(NutTabPane, {
+
+  const defaultSlot = () => {
+    // 确定 children
+    // slots 优先级高于 items
+    // 如果定义了 slots, items 失效
+    // @ts-ignore
+    const items: TabsItem[] = slots.default?.().map((s, index) => {
+      // @ts-ignore
+      const tabSlot = s.children?.tab?.()
+      return {
+        value: s.props?.key as string,
+        label: tabSlot || s.props?.tab || `Tab-${index}` as string,
+        content: s.children
+      }
+    }) || props.items || []
+
+    return items?.map(item => h(NutTabPane, {
         class: ['tab-pane'],
         paneKey: item.value || item.label,
         title: item.label
       }, item.content)
     )
+  }
 
   return h(NutTabs, {
     class: [
