810ed384e0cca7e7b0ead15db1f316d11f303b8c
xujian <mikejianxu@gmail.com>
2024-12-16 09:35:48 +0800

    packages upgrade

--- ui/babel.config.cjs
+++ ui/babel.config.cjs
@@ -11,26 +11,4 @@ module.exports = {
     }],
     '@babel/preset-typescript',
   ],
-  plugins: [
-    ['transform-define', {
-      __VERSION__: pack.version,
-      __REQUIRED_VUE__: pack.peerDependencies.vue,
-    }],
-  ],
-  env: {
-    test: {
-      presets: [
-        ['@babel/preset-env', {
-          targets: { node: true },
-          modules: 'commonjs',
-        }],
-      ],
-    },
-    lib: {
-      ignore: ['**/__tests__'],
-      plugins: [
-        ['babel-plugin-add-import-extension', { extension: 'mjs' }],
-      ],
-    },
-  },
 }
