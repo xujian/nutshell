41aef5a905f1e733a3761fa7cc6634e879f210a4
xujian <mikejianxu@gmail.com>
2024-12-20 16:52:25 +0800

    + <ns-popover> vendor

--- ui/src/components/popover/Popover.ts
+++ ui/src/components/popover/Popover.ts
@@ -1,7 +1,7 @@
-import { PropType, defineComponent, getCurrentInstance, h, onMounted, ref } from 'vue'
-import tippy, { type Placement } from 'tippy.js'
-import { MakePropsType } from '../../utils'
+import { PropType } from 'vue'
+import { define, MakePropsType } from '../../utils'
 import { useModelValuePropsForBoolean, useTriggerProps } from '../../props'
+
 
 export const popoverProps = {
   ...useModelValuePropsForBoolean(),
@@ -15,7 +15,7 @@ export const popoverProps = {
     require: false
   },
   placement: {
-    type: String as PropType<Placement>,
+    type: String,
     require: false
   },
   trigger: {
@@ -46,36 +46,11 @@ export type PopoverProps = MakePropsType<typeof popoverProps, PopoverEmits>
 /**
  *  气泡卡片组件 <ns-popover>
  */
-export const NsPopover = defineComponent({
+export const NsPopover = define({
   name: 'NsPopover',
   props: popoverProps,
   emits: popoverEmits,
-  setup(props, { slots }) {
-
-    const contentRef = ref()
-
-    onMounted(() => {
-      const vm = getCurrentInstance() as any
-      const panel = tippy(vm.parent.vnode.el, {
-        content: contentRef.value,
-        allowHTML: true,
-        duration: 300,
-        delay: [200, 20_000],
-        trigger: props.trigger || 'mouseenter',
-        interactive: true,
-        theme: 'light',
-        placement: props.placement || 'top',
-      })
-    })
-
-    return () => h('div', {
-      class: ['ns-popover-content'],
-      ref: contentRef,
-    }, [
-      h('div', {
-        class: ['ns-popover-title'],
-      }, props.title),
-      slots.default?.()
-    ])
+  setup (props, { slots }) {
+    return {}
   }
 })
