48deb380c339585e74aa429eb1e592c28b51e262
xujian <mikejianxu@gmail.com>
2025-01-06 09:28:44 +0800

    $n.dialog() + async complete

--- docs/src/stories/dialog/basic.vue
+++ docs/src/stories/dialog/basic.vue
@@ -10,6 +10,11 @@
       width="100"
       color="negtive"
       label="无按钮弹窗" @click="onOpenDialogNoButtonsButtonClick" />
+    <ns-button
+      size="sm"
+      width="100"
+      color="negtive"
+      label="异步回调方法" @click="onOpenDialogAsyncOnCompleteClick" />
   </ns-row>
 </template>
 
@@ -36,6 +41,20 @@ const onOpenDialogNoButtonsButtonClick = () => {
     footer: false,
     onOk: () => {
       return true
+    }
+  })
+}
+
+const onOpenDialogAsyncOnCompleteClick = () => {
+  $n.dialog({
+    title: '客户',
+    message: '消息内容',
+    onComplete: async () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(true)
+        }, 5000)
+      })
     }
   })
 }
