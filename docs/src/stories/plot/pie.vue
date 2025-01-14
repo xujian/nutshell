9c255266bdf90022aa79fc859da976c023274a91
gaozhu <gaozhu@ddjf.com.cn>
2025-01-14 17:56:44 +0800

    docs 注释 uxda/plot 功能

--- docs/src/stories/plot/pie.vue
+++ docs/src/stories/plot/pie.vue
@@ -1,7 +1,7 @@
 <template>
-  <ns-line-chart />
+  <!-- <ns-line-chart /> -->
 </template>
 
 <script lang="ts" setup>
-import { NsLineChart } from '@uxda/plot'
+// import { NsLineChart } from '@uxda/plot'
 </script>
