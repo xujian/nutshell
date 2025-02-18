df31f85d449802769ddbe062cd1ea4e72b002c3d
luoguan <luoguan@ddjf.com.cn>
2025-02-18 10:22:07 +0800

    feat: ns-checkbox添加disabled属性

--- expo/play/src/pages/components/checkbox/checkbox.vue
+++ expo/play/src/pages/components/checkbox/checkbox.vue
@@ -12,6 +12,11 @@
         </ns-card>
         <code-view language="html" :code="codes[0]" />
       </ns-form>
+      <h2>禁用状态</h2>
+      <ns-card body-fill="#ffffff">
+        <ns-checkbox v-model="formData.val3" disabled>未选时禁用状态</ns-checkbox>
+        <ns-checkbox v-model="formData.val4" disabled>选中时禁用状态</ns-checkbox>
+      </ns-card>
     </ns-page-content>
   </ns-page>
 </template>
@@ -20,14 +25,20 @@
 import { reactive } from 'vue'
 const formData = reactive({
   val1: '',
-  val2: ''
+  val2: '',
+  val3: false,
+  val4: true
 })
 
 const codes = [
 `<ns-card body-fill="#ffffff">
-  <ns-checkbox v-model="formData.val1">复选框</ns-checkbox>
-  <ns-checkbox v-model="formData.val2">复选框</ns-checkbox>
-</ns-card>`,
+    <ns-checkbox v-model="formData.val1">复选框</ns-checkbox>
+    <ns-checkbox v-model="formData.val2" text-position="right">复选框</ns-checkbox>
+  </ns-card>`,
+`<ns-card body-fill="#ffffff">
+  <ns-checkbox v-model="formData.val3" disabled>未选时禁用状态</ns-checkbox>
+  <ns-checkbox v-model="formData.val4" disabled>选中时禁用状态</ns-checkbox>
+</ns-card>`
 ]
 </script>
 
--- ui/src/vendors/nutui/components/checkbox/Checkbox.ts
+++ ui/src/vendors/nutui/components/checkbox/Checkbox.ts
@@ -8,6 +8,7 @@ export const Checkbox = (props: CheckboxProps, { slots }: Omit<SetupContext, 'ex
     label: props.label,
     modelValue: props.modelValue,
     textPosition: props.textPosition,
+    disabled: props.disabled,
     'onUpdate:modelValue': props['onUpdate:modelValue'],
   }, slots)
 }
