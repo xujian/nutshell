5ce5e326d18ecd1ed34664e47129dc8bfaf9afe3
luoguan <luoguan@ddjf.com.cn>
2025-02-18 17:55:14 +0800

    feat: ns-date-input增加代码展示

--- expo/play/src/pages/components/input/date-input.vue
+++ expo/play/src/pages/components/input/date-input.vue
@@ -10,15 +10,17 @@
           format="YYYY-MM-DD"
           placeholder="请选择日期"
           :model-value="formData.date1"
-          dense />
-        <h2>对齐方式(props.variant === 'solid' ? 'left' : 'right')</h2>
-          <ns-date-input
-            name="date2"
-            v-model="formData.date2"
-            label="日期"
-            variant="solid"
-            placeholder="请选择日期"
           />
+        <code-view language="html" :code="codes[0]" />
+        <h2>对齐方式(props.variant === 'solid' ? 'left' : 'right')</h2>
+        <ns-date-input
+          name="date2"
+          v-model="formData.date2"
+          label="日期"
+          variant="solid"
+          placeholder="请选择日期"
+        />
+        <code-view language="html" :code="codes[1]" />
         <h2>包含时间</h2>
         <ns-date-input name="date3" label="选择时间"
           placeholder="请选择时间"
@@ -26,6 +28,7 @@
           time-format="HH:mm"
           format="YYYY-MM-DD HH:mm"
           v-model="formData.date3" />
+        <code-view language="html" :code="codes[2]" />
       </ns-form>
   </ns-page-content>
   </ns-page>
@@ -38,16 +41,28 @@ const formData = reactive({
   date2: '',
   date3: ''
 })
+const codes = [
+`<ns-date-input name="date1" label="日期"
+  format="YYYY-MM-DD"
+  placeholder="请选择日期"
+  :model-value="formData.date1"
+  />`,
+`<ns-date-input
+  name="date2"
+  v-model="formData.date2"
+  label="日期"
+  variant="solid"
+  placeholder="请选择日期"
+/>`,
+`<ns-date-input name="date3" label="选择时间"
+  placeholder="请选择时间"
+  has-time
+  time-format="HH:mm"
+  format="YYYY-MM-DD HH:mm"
+  v-model="formData.date3" />`
+]
 </script>
 
 <style lang="scss">
-h2 {
-  margin-top: 30px;
-  margin-bottom: 10px;
-  font-size: 14px;
-  color: #909ca4;
-  padding: 0 10px;
-  font-weight: 400;
-}
 </style>
 
