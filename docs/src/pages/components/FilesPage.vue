a206bdba5bfcd7c423489f8a3aca3c5417861f07
gaozhu <gaozhu@ddjf.com.cn>
2025-02-25 10:48:03 +0800

    docs nf-files 优化

--- docs/src/pages/components/FilesPage.vue
+++ docs/src/pages/components/FilesPage.vue
@@ -8,13 +8,14 @@
       <note>文件的上传与展示</note>
       <p>&nbsp;</p>
       <h2>基本用法</h2>
+      <div class="caption">items - 文件列表，格式为[{id: string, name: string, type: string, url: string, thumb?: string}]</div>
+      <div class="caption" style="margin-top: -10px; margin-bottom: 15px;">downloadable - 文件是否可下载；deletable - 文件是否可删除</div>
       <story file="files/basic.vue" />
     </ns-page-content>
   </ns-page>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
 </script>
 
 <style lang="scss">
