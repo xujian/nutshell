694eec657a0af563226c04003899f48f8f6c8190
gaozhu <gaozhu@ddjf.com.cn>
2025-02-28 09:03:35 +0800

    docs v-tooltip/v-loading 标准化

--- docs/src/pages/directives/LoadingPage.vue
+++ docs/src/pages/directives/LoadingPage.vue
@@ -1,14 +1,16 @@
 <template>
   <ns-page class="loading-page">
-    <h1>区域加载 v-loading</h1>
-    <p class="caption">v-loading</p>
-    <p>&nbsp;</p>
-    <h2>基本用法</h2>
-    <div class="caption">v-loading="true"</div>
-    <story file="loading/basic" />
-    <p>&nbsp;</p>
-    <h2>可动态开启/关闭</h2>
-    <story file="loading/switch" />
+    <ns-page-content>
+      <h1>区域加载</h1>
+      <p class="caption">v-loading</p>
+      <p>&nbsp;</p>
+      <h2>基本用法</h2>
+      <div class="caption">v-loading="true"</div>
+      <story file="loading/basic" />
+      <p>&nbsp;</p>
+      <h2>可动态开启/关闭</h2>
+      <story file="loading/switch" />
+    </ns-page-content>
   </ns-page>
 </template>
 
--- docs/src/pages/directives/TooltipPage.vue
+++ docs/src/pages/directives/TooltipPage.vue
@@ -5,7 +5,7 @@
       <p class="caption">v-tooltip</p>
       <p>&nbsp;</p>
       <h2>基本用法</h2>
-      <div class="caption">接收字符串或带content的对象数据；如: v-tooltip="'客户标签'"或 v-tooltip="{ content: '客户标签' }"</div>
+      <div class="caption">其表现形式与popover类似；接收字符串或带content的对象数据；如: v-tooltip="'客户标签'"或 v-tooltip="{ content: '客户标签' }"</div>
       <story file="tooltip/basic" />
     </ns-page-content>
   </ns-page>
