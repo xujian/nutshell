d7130882a80201fbe422935b5b9aad1e4bb13f8e
xujian <mikejianxu@gmail.com>
2025-03-05 09:53:47 +0800

    fix: <ns-chip> default R

--- expo/play/src/pages/components/chip/chip.vue
+++ expo/play/src/pages/components/chip/chip.vue
@@ -7,21 +7,30 @@
       <color-cases class="breakout" v-slot="{color}">
         <ns-chip
           :color
-          label="确定" />
+          label="消耗" />
       </color-cases>
       <h2>Variants</h2>
       <variant-cases v-slot="{variant}" class="breakout column">
         <ns-chip v-for="(color) in colors"
           :key="color"
           :color
-          :variant label="创建订单" />
+          :variant label="收益" />
       </variant-cases>
+      <p>&nbsp;</p>
+      <h2>R角</h2>
+      <ns-row :gap="4">
+        <ns-chip v-for="(color) in colors"
+          :key="color"
+          :color
+          round label="收益" />
+      </ns-row>
+      <code-view :code="codes[0]" language="html" />
       <p>&nbsp;</p>
       <h2>可关闭</h2>
       <ns-chip
         color="primary"
         closable
-        label="确定" />
+        label="经营企业" />
     </ns-page-content>
   </ns-page>
 </template>
@@ -29,5 +38,12 @@
 <script lang="ts" setup>
 import { brands } from '@uxda/nutshell/taro'
 const colors = brands.slice(0, 3)
+
+const codes = [
+`<ns-chip v-for="(color) in colors"
+  :key="color"
+  :color
+  round label="收益" />`
+]
 </script>
 
