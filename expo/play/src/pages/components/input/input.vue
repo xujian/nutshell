e5cc1d0e8c42e22434622f2326021e849327175c
xujian <mikejianxu@gmail.com>
2025-03-07 16:16:44 +0800

    wrap input components in <ns-card>

--- expo/play/src/pages/components/input/input.vue
+++ expo/play/src/pages/components/input/input.vue
@@ -1,82 +1,99 @@
 <template>
   <ns-page class="input-page">
     <ns-page-header title="输入框"
+      fill="#ffffff88"
       caption="<ns-input>"
-      :blur="10" reveal has-back-button />
+      :blur="40" reveal has-back-button />
     <ns-page-content>
       <ns-form>
         <h2>基础用法</h2>
-        <ns-input
-          label="文本"
-          placeholder="请输入文本"
-          v-model="formData.val1" />
+        <ns-card fill="#fff">
+          <ns-input
+            label="姓名"
+            placeholder="请输入姓名"
+            v-model="formData.val1" />
+        </ns-card>
         <code-view language="html" :code="codes[0]" />
         <h2>禁用</h2>
-        <ns-input
-          label="文本"
-          placeholder="输入框已禁用"
-          v-model="formData.val2"
-          disabled
-        />
+        <ns-card fill="#fff">
+          <ns-input
+            label="姓名"
+            placeholder="输入框已禁用"
+            v-model="formData.val2"
+            disabled
+          />
+        </ns-card>
         <code-view language="html" :code="codes[1]" />
         <h2>插槽的使用</h2>
-        <ns-input
-          v-model="formData.val3"
-          label="文本"
-          placeholder="左侧label添加图标"
-        >
+        <ns-card fill="#fff">
+          <ns-input
+            v-model="formData.val3"
+            label="姓名"
+            placeholder="左侧label添加图标">
           <template #prepend>
             <ns-icon name="https://cdn.ddjf.com/static/images/wx-yunservice/search-icon.png" />
-          </template>
-        </ns-input>
-        <ns-input
-          v-model="formData.val4"
-          label="文本"
-          placeholder="右侧内容添加按钮"
-        >
+            </template>
+          </ns-input>
+        </ns-card>
+        <ns-card fill="#fff">
+          <ns-input
+            v-model="formData.val4"
+            label="姓名"
+            placeholder="右侧内容添加按钮">
           <template #append>
             <ns-button size="sm" color="neutral" variant="outlined">按钮</ns-button>
           </template>
         </ns-input>
+        </ns-card>
         <code-view language="html" :code="codes[2]" />
         <h2>长度限制(10)</h2>
-        <ns-input
-          v-model="formData.val5"
-          label="文本"
-          maxlength="10"
-          placeholder="请输入文本"
-        />
+        <ns-card fill="#fff">
+          <ns-input
+            v-model="formData.val5"
+            label="姓名"
+            name="name"
+            maxlength="10"
+            placeholder="请输入姓名"/>
+        </ns-card>
         <code-view language="html" :code="codes[3]" />
         <h2>对齐方式(props.variant === 'solid' ? 'left' : 'right')</h2>
-        <ns-input
+        <ns-card fill="#fff">
+          <ns-input
           v-model="formData.val6"
-          label="文本"
+          label="姓名"
+            name="name"
           variant="solid"
-          placeholder="输入框内容对齐方向"
-        />
+          placeholder="输入框内容对齐方向" />
+        </ns-card>
         <code-view language="html" :code="codes[4]" />
         <h2>身份证输入框</h2>
-        <ns-id-input
-          v-model.value="formData.val7"
-          placeholder="请输入证件号码"
-          name="id"
-          label="证件号码"/>
+        <ns-card fill="#fff">
+          <ns-id-input
+            v-model.value="formData.val7"
+            placeholder="请输入证件号码"
+            name="id"
+            label="证件号码"/>
+        </ns-card>
         <code-view language="html" :code="codes[5]" />
         <h2>数字输入框</h2>
-        <ns-number-input
-          label="数字"
-          name="phone"
-          placeholder="请输入"
-          v-model="formData.val8"
-        />
+        <ns-card fill="#fff">
+          <ns-number-input
+            label="年龄"
+            name="phone"
+            placeholder="请输入年龄"
+            v-model="formData.val8"
+          />
+        </ns-card>
         <code-view language="html" :code="codes[6]" />
         <h2>手机号输入框</h2>
-        <ns-mobile-input
-          label="手机号"
-          name="phone"
-          placeholder="请输入手机号"
-          v-model="formData.val9"
-        />
+        <ns-card fill="#fff">
+          <ns-mobile-input
+            label="手机号"
+            name="phone"
+            placeholder="请输入手机号"
+            v-model="formData.val9"
+          />
+        </ns-card>
         <code-view language="html" :code="codes[7]" />
       </ns-form>
     </ns-page-content>
@@ -99,18 +116,18 @@ const formData = reactive({
 
 const codes = [
 `<ns-input
-  label="文本"
-  placeholder="请输入文本"
+  label="姓名"
+  placeholder="请输入姓名"
   v-model="formData.val1" />`,
 `<ns-input
-  label="文本"
+  label="姓名"
   placeholder="输入框已禁用"
   v-model="formData.val2"
   disabled
 />`,
 `<ns-input
   v-model="formData.val3"
-  label="文本"
+  label="姓名"
   placeholder="左侧label添加图标"
 >
   <template #prepend>
@@ -119,7 +136,7 @@ const codes = [
 </ns-input>
 <ns-input
   v-model="formData.val4"
-  label="文本"
+  label="姓名"
   placeholder="右侧内容添加按钮"
 >
   <template #append>
@@ -128,13 +145,13 @@ const codes = [
 </ns-input>`,
 `<ns-input
   v-model="formData.val5"
-  label="文本"
+  label="姓名"
   maxlength="10"
-  placeholder="请输入文本"
+  placeholder="请输入姓名"
 />`,
 `<ns-input
   v-model="formData.val6"
-  label="文本"
+  label="姓名"
   variant="solid"
   placeholder="输入框内容对齐方向"
 />`,
--- expo/play/src/pages/components/input/textarea.vue
+++ expo/play/src/pages/components/input/textarea.vue
@@ -1,45 +1,55 @@
 <template>
   <ns-page class="input-page">
-    <ns-page-header title="文本域"
+    <ns-page-header title="多行输入框"
       caption="<ns-textarea>"
       :blur="10" reveal has-back-button />
     <ns-page-content>
       <ns-form>
         <h2>基础用法</h2>
-        <ns-textarea
-          label="文本"
-          placeholder="请输入"
-          v-model="formData.date1"
-          />
+        <ns-card fill="#fff">
+          <ns-textarea
+            label="文本"
+            placeholder="请输入"
+            v-model="formData.date1"
+            />
+        </ns-card>
         <code-view language="html" :code="codes[0]" />
         <h2>显示字数统计</h2>
-        <ns-textarea
-          label="文本"
-          placeholder="请输入"
-          v-model="formData.date2"
-          :maxlength="50"
-          hasCount />
+        <ns-card fill="#fff">
+          <ns-textarea
+            label="文本"
+            placeholder="请输入"
+            v-model="formData.date2"
+            :maxlength="50"
+            hasCount />
+        </ns-card>
         <code-view language="html" :code="codes[1]" />
         <h2>高度自动拉伸</h2>
-        <ns-textarea
-          placeholder="请输入"
-          v-model="formData.date3"
-          label="文本"
-          :autosize="true" />
+        <ns-card fill="#fff">
+          <ns-textarea
+            placeholder="请输入"
+            v-model="formData.date3"
+            label="文本"
+            :autosize="true" />
+        </ns-card>
         <code-view language="html" :code="codes[2]" />
         <h2>行数（仅支持H5）</h2>
-        <ns-textarea
-          placeholder="请输入"
-          v-model="formData.date4"
-          label="文本"
-          :rows="6"/>
+        <ns-card fill="#fff">
+          <ns-textarea
+            placeholder="请输入"
+            v-model="formData.date4"
+            label="文本"
+            :rows="6"/>
+        </ns-card>
         <code-view language="html" :code="codes[3]" />
         <h2>禁用</h2>
-        <ns-textarea
-          placeholder="请输入"
-          v-model="formData.date5"
-          label="文本"
-          :disabled="true"/>
+        <ns-card fill="#fff">
+          <ns-textarea
+            placeholder="请输入"
+            v-model="formData.date5"
+            label="文本"
+            :disabled="true"/>
+        </ns-card>
         <code-view language="html" :code="codes[4]" />
       </ns-form>
   </ns-page-content>
