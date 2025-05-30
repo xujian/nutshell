ac46d04b88629644c621d455a6dddb99e68126ca
xujian <mikejianxu@gmail.com>
2025-05-30 15:49:47 +0800

    <pge-content> applied NsScrollable

--- ui/src/components/page/PageContent.ts
+++ ui/src/components/page/PageContent.ts
@@ -3,13 +3,20 @@ import { MakePropsType } from '../../utils'
 import { useBus } from '../../composables'
 import { usePage } from './Page'
 import { buildDesignClasses, buildDesignStyles, useDesignProps } from '../../props'
-// import { ScrollView } from '@tarojs/components'
+import { NsScrollable } from '../scrollable'
 
 export const pageContentProps = {
   /**
    * Page content 允许滚动
    */
   scrollable: {
+    type: Boolean,
+    default: false,
+  },
+  /**
+   * 是否可下拉刷新
+   */
+  refreshable: {
     type: Boolean,
     default: false,
   },
@@ -28,10 +35,15 @@ export type PageContentEmits = {
    * 可滚动部件达到底部
    */
   bottomReached: () => void
+  /**
+   * 下拉刷新
+   */
+  pullDownRefresh: () => void
 }
 
 export const pageContentEmits: PageContentEmits = {
-  bottomReached: () => true
+  bottomReached: () => true,
+  pullDownRefresh: () => true
 }
 
 export type PageContentProps = MakePropsType<typeof pageContentProps, PageContentEmits>
@@ -53,21 +65,19 @@ export const NsPageContent = defineComponent({
       })
     }
 
-    const renderScrollview = (content: any) => h('scroll-view', {
+    const renderScrollview = (content: any) => h(NsScrollable, {
         class: ['page-content-scroll-view', 'scroll'],
         'scroll-y': true,
+        refreshable: props.refreshable,
         onScroll,
-        lowerThreshold: 50,
-        onScrolltoupper () {
-          // console.log('===onScrollToUpper===')
+        onRefresh () {
+          emit('pullDownRefresh')
         },
-        onScrolltolower () {
-          // console.log('===bindscrolltolower===')
-          // console.log('===onScrolltolower===', props)
+        onBottomReached () {
           emit('bottomReached')
         }
       },
-      h('div', {
+      () => h('div', {
         class: 'page-content-scroll-content'
       }, content)
     )
