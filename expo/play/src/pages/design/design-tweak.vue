f2019e033d8bcddda77d7a1e74588351e0793551
xujian <mikejianxu@gmail.com>
2025-06-17 14:35:25 +0800

    + expo/play design pages

---
+++ expo/play/src/pages/design/design-tweak.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="design-tweak-container">
+    <ns-card
+      class="design-tweak-card"
+      title="广发银行 - 小微贷"
+      :color-scheme="scheme"
+      :fill
+      :stroke
+      :blur
+      :brightness
+      :shadow
+      :depth
+      foreground="#fff">
+      <ns-number
+        :value="4"
+        suffix="天"
+        footer="最快放款时长"
+        align="start" />
+      <ns-chips :options="tags.map(t => ({value: t, label: t})) || []"
+        color="primary" />
+    </ns-card>
+    <ns-row class="design-row justify-between">
+      <div class="label">color scheme</div>
+      <ns-button-group v-model="scheme" color="primary" :items="schemes" />
+    </ns-row>
+    <ns-row class="design-row justify-between">
+      <div class="label">fill</div>
+      <ns-button-group v-model="fill" color="primary" :items="fills" />
+    </ns-row>
+    <ns-row class="design-row justify-between">
+      <div class="label">stroke</div>
+      <ns-button-group v-model="stroke" color="primary" :items="strokes" />
+    </ns-row>
+    <ns-row class="design-row justify-between">
+      <div class="label">blur</div>
+      <ns-button-group v-model="blur" color="primary" :items="blurs" />
+    </ns-row>
+    <ns-row class="design-row justify-between">
+      <div class="label">brightness</div>
+      <ns-button-group v-model="brightness"
+        color="primary"
+        :items="brightnesses" />
+    </ns-row>
+    <ns-row class="design-row justify-between">
+      <div class="label">shadow</div>
+      <ns-button-group v-model="shadow"
+        color="primary"
+        :items="shadows" />
+    </ns-row>
+    <ns-row class="design-row justify-between">
+      <div class="label">depth</div>
+      <ns-button-group v-model="depth"
+        color="primary"
+        :items="depthes" />
+    </ns-row>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const tags = ['放款快', '利率低', '支持提放保', '简易审核']
+
+const
+  schemes = ['light', 'dark']
+    .map(c => ({label: c, value: c})),
+  fills = ['#ffffff22', '#ffffff44', '#ffffff88', '#00000088']
+    .map(c => ({label: c, value: c})),
+  strokes = ['#ffffff66', '#ffffff99', '#ffffff33']
+    .map(c => ({label: c, value: c})),
+  blurs = [5, 10, 15, 20, 30, 40, 50, 75, 100]
+    .map(c => ({label: `${c}`, value: c})),
+  brightnesses = [1, 1.2, 1.5, 2]
+    .map(c => ({label: `${c}`, value: c})),
+  shadows = ['#00000044', '#ffffff44']
+    .map(c => ({label: c, value: c})),
+  depthes = [5, 10, 15, 20]
+    .map(c => ({label: `${c}`, value: c}))
+
+const scheme = ref(schemes[0].value),
+  fill = ref(fills[0].value),
+  stroke = ref(strokes[0].value),
+  blur = ref(blurs[0].value),
+  brightness = ref(brightnesses[0].value),
+  shadow = ref(shadows[0].value),
+  depth = ref(depthes[0].value)
+
+</script>
+
+<style lang="scss">
+.design-row {
+  background-color: var(--ns-primary--50);
+  padding-inline-start: var(--ns-spacing);
+  border-radius: var(--ns-r);
+  margin: 10px 0;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+</style>
---
+++ expo/play/src/pages/design/props.vue
@@ -0,0 +1,127 @@
+<template>
+  <ns-page class="props-page" fill="neutral" color-scheme="dark">
+    <ns-page-header title="视觉风格" fill="#00000044" reveal :blur="50" />
+    <ns-page-content>
+      <h3>视觉风格调试器</h3>
+      <design-tweak />
+      <callout>
+        <p>视觉风格是现代UI设计的核心, 赏心悦目的视觉风格来自于色彩、线条、字型、透明度、渐变、动画的组合与映衬。</p>
+      </callout>
+      <h3>视觉风格属性总表</h3>
+      <props-table
+        :data="props" />
+      <p>&nbsp;</p>
+      <ns-grid :columns="2" gap>
+        <ns-card fill="#fff" body-fill="#ffffff22"
+          :edge="2"
+          foreground="black">
+          <h3>色彩</h3>
+        </ns-card>
+        <ns-card fill="#fff" body-fill="#ffffff22"
+          :edge="2"
+          foreground="black">
+          <h3>色彩</h3>
+        </ns-card>
+        <ns-card fill="#fff" body-fill="#ffffff22"
+          :edge="2"
+          foreground="black">
+          <h3>色彩</h3>
+        </ns-card>
+        <ns-card fill="#fff" body-fill="#ffffff22"
+          :edge="2"
+          foreground="black">
+          <h3>色彩</h3>
+        </ns-card>
+      </ns-grid>
+    </ns-page-content>
+  </ns-page>
+</template>
+
+<script lang="ts" setup>
+import DesignTweak from './design-tweak.vue'
+
+const props = [
+  {
+    name: '填色',
+    description: ':fill',
+    type: 'Color',
+    default: ''
+  },
+  {
+    name: '面色', 
+    description: ':surface',
+    type: 'Color',
+    default: ''
+  },
+  {
+    name: '字色',
+    description: ':foreground', 
+    type: 'Color',
+    default: ''
+  },
+  {
+    name: '渐变底色',
+    description: ':gradient',
+    type: 'Color',
+    default: ''
+  },
+  {
+    name: '底图',
+    description: ':texture',
+    type: 'string',
+    default: ''
+  },
+  {
+    name: '图案',
+    description: ':pattern',
+    type: 'string',
+    default: ''
+  },
+  {
+    name: '圆角',
+    description: ':round',
+    type: 'string',
+    default: ''
+  },
+  {
+    name: '倒角',
+    description: ':radius',
+    type: 'string',
+    default: ''
+  },
+  {
+    name: '线色',
+    description: ':stroke',
+    type: 'string',
+    default: ''
+  },
+  {
+    name: '线宽',
+    description: ':thick',
+    type: 'string',
+    default: ''
+  },
+  {
+    name: '模糊',
+    description: ':blur',
+    type: 'string',
+    default: ''
+  },
+  {
+    name: '提亮',
+    description: ':brightness',
+    type: 'string',
+    default: ''
+  },
+]
+
+</script>
+
+
+<style lang="scss">
+.props-page {
+  background-image: url(https://simple.shensi.tech/images/ios26.jpg);
+  background-size: cover;
+  box-sizing: border-box;
+}
+</style>
